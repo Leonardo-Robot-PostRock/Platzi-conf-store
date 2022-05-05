d9874a272b858321bc39f7026641051852191da3
Leonardo Puebla <leonardo38361@gmail.com>
2022-05-05 01:56:34 -0300

    Se installa react router dom, se crean componentes y se agregan a las rutas. Se agrega nueva configuración en webpack para las rutas

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 import '../styles/components/App.css';
 
-const App = () => {
-  <h1>Hola Mundo</h1>;
+const App =  () => {
+    return (
+        <h1>Hola Mundo</h1>
+    );
 };
 
 export default App;
---
+++ src/routes/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from '../container/Home';
+import Checkout from '../container/Checkout';
+import Information from '../container/Information';
+import Payment from '../container/Payment';
+import Success from '../container/Success';
+import NotFound from '../container/NotFound';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/checkout/information*" element={<Information />} />
+        <Route path="/checkout/payment*" element={<Payment />} />
+        <Route path="/checkout/success*" element={<Success />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
