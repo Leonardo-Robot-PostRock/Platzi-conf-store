32ecd72365ad8bcfd981732d9be611dc794e25fd
Leonardo Puebla <leonardo38361@gmail.com>
2022-05-05 13:04:23 -0300

    Maquetación y estilos del home

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -6,18 +6,21 @@ import Information from '../container/Information';
 import Payment from '../container/Payment';
 import Success from '../container/Success';
 import NotFound from '../container/NotFound';
+import Layout from '../components/Layout';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/checkout/information*" element={<Information />} />
-        <Route path="/checkout/payment*" element={<Payment />} />
-        <Route path="/checkout/success*" element={<Success />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/checkout/information*" element={<Information />} />
+          <Route path="/checkout/payment*" element={<Payment />} />
+          <Route path="/checkout/success*" element={<Success />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 };
