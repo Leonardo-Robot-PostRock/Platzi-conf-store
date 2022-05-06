ce6326d84a0dd9c8a9b126c8d52b927ed85c0a9c
Leonardo Puebla <leonardo38361@gmail.com>
2022-05-06 01:50:15 -0300

    Maquetación y estilos de información de usuario y checkout

--- src/container/Information.jsx
+++ src/container/Information.jsx
@@ -1,8 +1,45 @@
 import React from 'react';
+import '../styles/components/Information.css'
 
 const Information = () => {
     return (
-        <h1>Information</h1>
+        <div className="Information">
+            <div className="Information-content">
+                <div className="Information-head">
+                    <h2>Información de contacto</h2>
+                </div>
+                <div className="Information-form">
+                    <form action="">
+                        <input type="text" placeholder='Nombre completo' name="nombre" id="" />
+                        <input type="text" placeholder='Correo Electronico' name="mail" id="" />
+                        <input type="text" placeholder='Dirección' name="address" id="" />
+                        <input type="text" placeholder='Apto' name="apto" id="" />
+                        <input type="text" placeholder='Ciudad' name="city" id="" />
+                        <input type="text" placeholder='País' name="country" id="" />
+                        <input type="text" placeholder='Estado' name="state" id="" />
+                        <input type="text" placeholder='Código postal' name="cp" id="" />
+                        <input type="text" placeholder='Teléfono' name="phone" id="" />
+                    </form>
+                </div>
+                <div className="Information-buttons">
+                    <div className="Information-back">
+                        Regresar
+                    </div>
+                    <div className="Information-next">
+                        Pagar
+                    </div>
+                </div>
+                <div className="Information-sidebar">
+                    <h3>Pedido:</h3>
+                    <div className="Information-item">
+                        <div className="Information-element">
+                            <h4>ITEM Name</h4>
+                            <span>$10</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
