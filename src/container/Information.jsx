27d6a2785909dbffd62913257e3963f5713ff90a
Leonardo Puebla <leonardo38361@gmail.com>
2022-05-06 02:00:11 -0300

    Se agrega label para cada input

--- src/container/Information.jsx
+++ src/container/Information.jsx
@@ -1,46 +1,84 @@
 import React from 'react';
-import '../styles/components/Information.css'
+import '../styles/components/Information.css';
 
 const Information = () => {
-    return (
-        <div className="Information">
-            <div className="Information-content">
-                <div className="Information-head">
-                    <h2>Información de contacto</h2>
-                </div>
-                <div className="Information-form">
-                    <form action="">
-                        <input type="text" placeholder='Nombre completo' name="nombre" id="" />
-                        <input type="text" placeholder='Correo Electronico' name="mail" id="" />
-                        <input type="text" placeholder='Dirección' name="address" id="" />
-                        <input type="text" placeholder='Apto' name="apto" id="" />
-                        <input type="text" placeholder='Ciudad' name="city" id="" />
-                        <input type="text" placeholder='País' name="country" id="" />
-                        <input type="text" placeholder='Estado' name="state" id="" />
-                        <input type="text" placeholder='Código postal' name="cp" id="" />
-                        <input type="text" placeholder='Teléfono' name="phone" id="" />
-                    </form>
-                </div>
-                <div className="Information-buttons">
-                    <div className="Information-back">
-                        Regresar
-                    </div>
-                    <div className="Information-next">
-                        Pagar
-                    </div>
-                </div>
-                <div className="Information-sidebar">
-                    <h3>Pedido:</h3>
-                    <div className="Information-item">
-                        <div className="Information-element">
-                            <h4>ITEM Name</h4>
-                            <span>$10</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="Information">
+      <div className="Information-content">
+        <div className="Information-head">
+          <h2>Contact Information:</h2>
         </div>
-    );
-}
+        <div className="Information-form">
+          <form action="">
+            <label htmlFor="name">
+              Full Name
+              <input
+                type="text"
+                placeholder="Full Name"
+                name="name"
+                id="name"
+              />
+            </label>
+            <label htmlFor="email">
+              Email
+              <input type="text" placeholder="Email" name="email" id="email" />
+            </label>
+            <label htmlFor="address">
+              Address
+              <input
+                type="text"
+                placeholder="Address"
+                name="address"
+                id="address"
+              />
+            </label>
+            <label htmlFor="apto">
+              APT
+              <input type="text" placeholder="APT" name="apto" id="apto" />
+            </label>
+            <label htmlFor="country">
+              Country
+              <input
+                type="text"
+                placeholder="Country"
+                name="country"
+                id="country"
+              />
+            </label>
+            <label htmlFor="state">
+              State
+              <input type="text" placeholder="State" name="state" id="state" />
+            </label>
+            <label htmlFor="city">
+              City
+              <input type="text" placeholder="City" name="city" id="city" />
+            </label>
+            <label htmlFor="cp">
+              Postal Code
+              <input type="text" placeholder="Postal Code" name="cp" id="cp" />
+            </label>
+            <label htmlFor="phone">
+              Phone
+              <input type="text" placeholder="Prone" name="phone" id="phone" />
+            </label>
+          </form>
+        </div>
+        <div className="Information-buttons">
+          <div className="Information-back">Go Back</div>
+          <div className="Information-next">Pay</div>
+        </div>
+      </div>
+      <div className="Information-sidebar">
+        <h3>Order Summary:</h3>
+        <div className="Information-item">
+          <div className="Information-element">
+            <h4>ITEM Name</h4>
+            <span>$10</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Information;
