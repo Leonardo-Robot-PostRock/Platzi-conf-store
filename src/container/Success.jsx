2ac29a363a4c6a8af46b8ce3e56220f5ebe8faa9
Leonardo Puebla <leonardo38361@gmail.com>
2022-05-13 03:35:48 -0300

    Se corrige la linea 13

--- src/container/Success.jsx
+++ src/container/Success.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '../context/AppContext';
+import Map from '../components/Map';
+import '../styles/components/Success.css';
 
-const Success = () => (
-    <h1>Success</h1>
-)
+const Success = () => {
+  const { state} = useContext(AppContext);
+    const { buyer } = state;
+
+  return (
+    <div className="Success">
+      <div className="Success-content">
+        <h2>{`${buyer[0].name}, Gracias por tu compra`}</h2>
+        <span>Tu pedido llegará en tres días a tu dirección</span>
+        <div className="Success-map">
+          <Map />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Success;
