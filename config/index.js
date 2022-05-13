e97eb3535ff88e5f0bf241c1b122416bd840911a
Leonardo Puebla <leonardo38361@gmail.com>
2022-05-13 03:32:50 -0300

    Variable de entorno que no se usa por lo que no puedo pagar la API

--- config/index.js
+++ config/index.js
@@ -1,5 +1,6 @@
 const config = {
-    clientIdPaypal: String(process.env.REACT_APP_CLIENT_ID)
+    clientIdPaypal: String(process.env.REACT_APP_CLIENT_ID),
+    googleMapsAPI: String(process.env.GOOGLE_MAPS_API)
 }
 
 export default config
