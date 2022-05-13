2dd62075dde20c665363b70dd03d8bddd46f5a6f
Leonardo Puebla <leonardo38361@gmail.com>
2022-05-13 03:31:03 -0300

    Se crea componente Map

---
+++ src/components/Map.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+// import { Icon } from 'leaflet'
+import '../styles/components/Map.css';
+
+const Icon =
+  'https://i.pinimg.com/474x/36/dd/16/36dd1601f1b0e6642b3556d827e1c0aa.jpg';
+
+const position = [51.505, -0.09];
+
+const Map = () => (
+  <MapContainer center={position} zoom={13}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={position}>
+      <Popup icon={Icon}>
+        A pretty CSS3 popup.
+        <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>
+);
+export default Map;
