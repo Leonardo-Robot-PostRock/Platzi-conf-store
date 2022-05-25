22b1e6f46919250adee239c7e647bd18d189d420
Leonardo Puebla <leonardo38361@gmail.com>
2022-05-25 03:01:02 -0300

    Se importa el componente MetaHead en Home.jsx

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import Products from '../components/Products';
 import initialState from '../initialState';
+import MetaHead from '../components/MetaHead';
 
-const Home = () => <Products products={initialState.products} />;
+const meta = (
+  <MetaHead
+    title="Productos"
+    description="Encuentra todos tus productos favoritos"
+    image="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+    url="https://platziConfMerch.xyz/"
+  />
+);
 
+const Home = () => (
+  <>
+    {meta}
+    <Products products={initialState.products} />
+  </>
+);
 export default Home;
