600b955f95e057958b7c200130f987a704fa817b
Leonardo Puebla <leonardo38361@gmail.com>
2022-05-28 04:40:13 -0300

    Se coloca la props en img. Se crea una pegada de API

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 
-const Product = ({ product, handleAddToCart }) => (
-  <div className="Products-item">
-    <img src={product.image} alt={product.title} />
-    <div className="Products-item-info">
-      <h2>
-        {product.title}
-        <span>${product.price}</span>
-      </h2>
-      <p>{product.description}</p>
+const Product = ({ product, image, handleAddToCart }) => {
+
+  return (
+    <div className="Products-item">
+        <img key={image.hash} src={`http://localhost:1337${image}`} alt={image.name} />
+      <div className="Products-item-info">
+        <h2>
+          {product.title}
+          <span>${product.price}</span>
+        </h2>
+        <p>{product.description}</p>
+      </div>
+      <button type="button" onClick={handleAddToCart(product)}>
+        Comprar
+      </button>
     </div>
-    <button type="button" onClick={handleAddToCart(product)}>
-      Comprar
-    </button>
-  </div>
-);
+  );
+};
 
 export default Product;
