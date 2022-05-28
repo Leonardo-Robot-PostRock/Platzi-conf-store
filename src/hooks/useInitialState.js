f966a8582f783725bf04c2fccdb4c1d84a95188e
Leonardo Puebla <leonardo38361@gmail.com>
2022-05-28 04:33:15 -0300

    Se crean estados para almacenar las respuestas de la API

--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -1,9 +1,42 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import config from '../../config/index';
 import initialState from '../initialState';
 
+const apiStrapiPlatzi = config.strapiPlatzi;
 
 const useInitialState = () => {
     const [state, setState] = useState(initialState);
+    const [products, setProducts] = useState([]);
+    const [image, setImage] = useState([]);
+    const urlImgStrapi = 'http://localhost:1337/api/upload/files';
+
+    useEffect(() => {
+        const getData = async () => {
+            const config= {
+                url:{
+                    url: ['url']
+                }
+            };
+            try {
+                axios.all([
+                    axios.get(apiStrapiPlatzi),
+                    axios.get(urlImgStrapi, config)
+                ])
+                    .then(response => {
+                        setProducts(response[0].data.data);
+                        setImage(response[1].data);
+                    })
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        getData();
+    }, []);
+
+    console.log(products);
+    console.log("ver", image);
+
 
     const addToCart = (payload) => {
         setState({
@@ -38,6 +71,8 @@ const useInitialState = () => {
         removeFromCart,
         addToBuyer,
         addNewOrder,
+        products,
+        image,
         state
     }
 }
