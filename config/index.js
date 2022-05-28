f3864624a505daf0ae3a6ad5c1d328821fa175f7
Leonardo Puebla <leonardo38361@gmail.com>
2022-05-28 04:41:50 -0300

    Se agrega env de strapi

--- config/index.js
+++ config/index.js
@@ -1,6 +1,7 @@
 const config = {
     clientIdPaypal: String(process.env.REACT_APP_CLIENT_ID),
-    googleMapsAPI: String(process.env.GOOGLE_MAPS_API)
+    googleMapsAPI: String(process.env.GOOGLE_MAPS_API),
+    strapiPlatzi: String(process.env.REACT_APP_STRAPI_PLATZI),
 }
 
 export default config
